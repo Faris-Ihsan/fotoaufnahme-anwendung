a4e66e892a1578197c4c5fc2fe7b4dead1e78ff4
Faris-Ihsan <farisihsan32@yahoo.co.id>
2025-04-15 19:17:08 +0700

    react-webcam photo capture

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,26 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { React, useState, useRef, useCallback} from "react";
+import Webcam from "react-webcam";
 
 function App() {
-    const [count, setCount] = useState(0);
+    const webcamRef = useRef(null);
+    const [imageSrc, setImgSrc] = useState(null);
+    const capture = useCallback(
+    () => {
+        const imageSrc = webcamRef.current.getScreenshot();
+        setImgSrc(imageSrc)
+    }, [webcamRef]
+    )
 
     return (
-        <>
-            <div>
-                <a href="https://vite.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img
-                        src={reactLogo}
-                        className="logo react"
-                        alt="React logo"
-                    />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.jsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
-        </>
+        <div>
+            <h1>fotoaufnahme</h1>
+                <Webcam
+                ref={webcamRef}
+                screenshotFormat="image/jpeg"
+                />
+                <button onClick={capture}>Capture</button>
+                <img src={imageSrc} alt="capture" />
+        </div>
     );
 }
 
