31225ba6bcc66ed04503904590378fcbb7cbfe04
Faris-Ihsan <farisihsan32@yahoo.co.id>
2025-04-21 15:26:05 +0700

    Erkennung von Dokumenten im Bild

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
     const webcamRef = useRef(null);
     const canvasRef = useRef(null);
 
-    // Variabel zu den Komponenten hinzugugen
+    // Variabel zu den Komponenten hinzufugen
     const [cvReady, setCvReady] = useState(false);
     const [imageSrc, setImgSrc] = useState(null);
 
@@ -21,7 +21,7 @@ function App() {
         setImgSrc(imageSrc);
     }, [webcamRef]);
 
-     // useEffect() => nach dem Render laufen (für API, openCv.Js) 
+     // useEffect() => nach dem Render laufen (für API, openCv.Js), ohne Re-Render
      // openCV.js Laden
     useEffect(() => {
         const checkOpenCV = () => {
@@ -34,101 +34,80 @@ function App() {
         checkOpenCV();
     }, []);
 
-    // Video analysieren
-    useEffect(() => {
-        let animationFrameId;
-
-        const realTimeProcess = () => {
-            if (
-                cvReady &&
-                webcamRef.current &&
-                webcamRef.current.video.readyState === 4
-            ) {
-                const video = webcamRef.current.video;
-                const canvas = canvasRef.current;
-                const ctx = canvas.getContext("2d");
-
-                canvas.width = video.videoWidth;
-                canvas.height = video.videoHeight;
-                ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-
-                const src = cv.imread(canvas);
-                const gray = new cv.Mat();
-                const blurred = new cv.Mat();
-                const edged = new cv.Mat();
-                const contours = new cv.MatVector();
-                const hierarchy = new cv.Mat();
-
-                // einer Funktion zur Analyse des Videos.
-                cv.cvtColor(src, gray, cv.COLOR_RGBA2GRAY);
-                cv.GaussianBlur(gray, blurred, new cv.Size(5, 5), 0, 0, cv.BORDER_DEFAULT);
-                cv.Canny(blurred, edged, 75, 200);
-                cv.findContours(edged, contours, hierarchy, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE);
-
-                let maxArea = 0;
-                let bestContour = null;
-
-                for (let i = 0; i < contours.size(); i++) {
-                    const cnt = contours.get(i);
-                    const area = cv.contourArea(cnt);
-                    const approx = new cv.Mat();
-                    cv.approxPolyDP(cnt, approx, 0.02 * cv.arcLength(cnt, true), true);
-                    if (area > 1000 && approx.rows === 4 && area > maxArea) {
-                        maxArea = area;
-                        if (bestContour) bestContour.delete();
-                        bestContour = approx;
-                    } else {
-                        approx.delete();
-                    }
-                }
-                
-                ctx.clearRect(0, 0, canvas.width, canvas.height);
-                ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-
-                // rotes Rechteck zeichnen
-                if (bestContour) {
-                    ctx.beginPath();
-                    ctx.strokeStyle = "red";
-                    ctx.lineWidth = 3;
-
-                    const points = [];
-                    for (let i = 0; i < 4; i++) {
-                        points.push({
-                            x: bestContour.intPtr(i, 0)[0],
-                            y: bestContour.intPtr(i, 0)[1],
-                        });
-                    }
-
-                    for (let i = 0; i < 4; i++) {
-                        const pt1 = points[i];
-                        const pt2 = points[(i + 1) % 4];
-                        ctx.moveTo(pt1.x, pt1.y);
-                        ctx.lineTo(pt2.x, pt2.y);
-                    }
-
-                    ctx.stroke();
-                    bestContour.delete();
-                }
-
-                src.delete();
-                gray.delete();
-                blurred.delete();
-                edged.delete();
-                contours.delete();
-                hierarchy.delete();
+    // einer Funktion zur Analyse des Bildes.
+    const processDocument = (imgElement) => {
+        //https://www.dynamsoft.com/codepool/web-document-scanner-with-opencvjs.html
+        const src = cv.imread(imgElement); 
+        const gray = new cv.Mat(); // neue Matrix machen
+        const blurred = new cv.Mat();
+        const edged = new cv.Mat();
+        const contours = new cv.MatVector();
+        const hierarchy = new cv.Mat();
+    
+        cv.cvtColor(src, gray, cv.COLOR_RGBA2GRAY); //die Farbe des Bildes umtauchsen
+        cv.GaussianBlur(gray, blurred, new cv.Size(5, 5), 0, 0, cv.BORDER_DEFAULT); //unscharfes Bild machen
+        cv.Canny(blurred, edged, 75, 200); //Linie vom Objekt suchen
+        cv.findContours(edged, contours, hierarchy, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE); // Kontur vom Objekt analysieren.
+    
+        // beste Kontur suchen
+        let maxArea = 0;
+        let bestContour = null;
+    
+        for (let i = 0; i < contours.size(); i++) {
+            const cnt = contours.get(i);
+            const area = cv.contourArea(cnt);
+            const approx = new cv.Mat();
+            cv.approxPolyDP(cnt, approx, 0.02 * cv.arcLength(cnt, true), true);
+            if (area > 1000 && approx.rows === 4 && area > maxArea) {
+                maxArea = area;
+                if (bestContour) bestContour.delete();
+                bestContour = approx;
+            } else {
+                approx.delete();
             }
-
-            animationFrameId = requestAnimationFrame(realTimeProcess);
-        };
-
-        if (cvReady) {
-            animationFrameId = requestAnimationFrame(realTimeProcess);
         }
+    
+        // Rechteck machen
+        if (bestContour) {
+            const points = [];
+            for (let i = 0; i < 4; i++) {
+                points.push({
+                    x: bestContour.intPtr(i, 0)[0],
+                    y: bestContour.intPtr(i, 0)[1],
+                });
+            }
+    
+            // Linie machen
+            for (let i = 0; i < 4; i++) {
+                const pt1 = points[i];
+                const pt2 = points[(i + 1) % 4];
+                cv.line(src, new cv.Point(pt1.x, pt1.y), new cv.Point(pt2.x, pt2.y), new cv.Scalar(255, 0, 0, 255), 3);
+            }
+    
+            bestContour.delete();
+        }
+    
+        // Bild anzeigen
+        cv.imshow(canvasRef.current, src);
+    
+        // Matrix löschen
+        src.delete();
+        gray.delete();
+        blurred.delete();
+        edged.delete();
+        contours.delete();
+        hierarchy.delete();
+    };
 
-        return () => {
-            cancelAnimationFrame(animationFrameId);
-        };
-    }, [cvReady]);
+    useEffect(() => {
+        if (imageSrc && cvReady) {
+            const img = new Image();
+            img.src = imageSrc;
+            img.onload = () => {
+                processDocument(img);
+            };
+        }
+    }, [imageSrc, cvReady]);
 
     return (
         <div
@@ -144,7 +123,6 @@ function App() {
                     <div className={"leading-relaxed p-4 "}>
                         {/* For Webcam Use */}
                         <Webcam ref={webcamRef} videoConstraints={videoConstraints} mirrored="false" screenshotFormat="image/jpeg" />
-                        <canvas ref={canvasRef} width={videoConstraints.width} height={videoConstraints.height}></canvas>
                     </div>
                     {/* Geschweifteklamme => ""
                         Gänzefüsschen => {}
@@ -157,6 +135,7 @@ function App() {
                     <div className={"leading-relaxed p-4 "}>
                         {/* Show captured images */}
                         <img className={"w-md"} src={imageSrc} alt="capture" />
+                        <canvas ref={canvasRef}></canvas>
                     </div>
                 </div>
             </div>
